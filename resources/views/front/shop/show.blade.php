ef9d7debe25329c5493a64aba25285764fdf4fab
LeThiHuyenTrang-2248 <lethihtrang998877@gmail.com>
2025-11-29 21:56:03 +0700

    component cho bo loc SP- hoan thien

--- resources/views/front/shop/show.blade.php
+++ resources/views/front/shop/show.blade.php
@@ -12,7 +12,7 @@
             <div class="col-lg-3 col-md-6 col-sm-8 order-2 order-lg-1 produts-sidebar-filter">
 
                 @include('front.shop.components.products-sidebar-filter')
-                
+
             </div>
             <div class="col-lg-9 order-1 order-lg-2">
                 <div class="row">
@@ -302,43 +302,9 @@
         <div class="row">
 
         @foreach ($relatedProducts as $product)
-             <div class="col-lg-3 col-sm-6">
-                <div class="product-item">
-                    <div class="pi-pic">
-                        <img src="front/img/products/{{ $product->productImages[0]->path }}" alt="">
-                        
-                        @if($product->discount != null)
-                            <div class="sale pp-sale">Sale</div>
-                        @endif
-
-                        <div class="icon">
-                            <i class="icon_heart_alt"></i>
-                        </div>
-                        <ul>
-                            <li class="w-icon active"><a href="#"><i class="icon_bag_alt"></i></a></li>
-                            <li class="quick-view"><a href="shop/product/{{ $product->id }}">+ Quick View</a></li>
-                            <li class="w-icon"><a href=""><i class="fa fa-random"></i></a></li>
-                        </ul>
-                    </div>
-                    <div class="pi-text">
-                        <div class="catagory-name">{{ $product->tag }}</div>
-                        <a href="shop/product/{{ $product->id }}">
-                            <h5>{{ $product->name }}</h5>
-                        </a>
-                        <div class="product-price">
-                            @if($product->discount != null)
-                                ${{ $product->discount }}
-                                <span>${{ $product->price }}</span>
-                            @else
-                                ${{ $product->price }}
-                            @endif
-                        </div>
-                    </div>
-                </div>
-            </div>
+             @include('front.components.product-item')
         @endforeach
            
-        
                 
             </div>
         </div>
